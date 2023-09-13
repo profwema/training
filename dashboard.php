844fe199e28424405a510ad6e5d7ac26e841d132
weaam <profwema@gmail.com>
2023-09-13 13:55:27 +0200

    ابديت الشغل 13-9

--- dashboard.php
+++ dashboard.php
@@ -33,44 +33,44 @@ if (isset($_GET['putAtGroup'])) {
 
 
 
-  <div id="page_wrapper">
-    <?php require_once("header.php"); ?>
+    <div id="page_wrapper">
+        <?php require_once("header.php"); ?>
 
-    <div class="page" style="min-height: 800px;">
-      <nav aria-label="breadcrumb" class='bread'>
-        <div class="container">
-          <ol class="breadcrumb">
-            <li class="breadcrumb-item"><a href="index.php">الرئيسية </a></li>
-            <li class="breadcrumb-item"><span> <?= $_SESSION['ar_name'] ?> : لوحة القيادة </span></li>
+        <div class="page" style="min-height: 800px;">
+            <nav aria-label="breadcrumb" class='bread'>
+                <div class="container">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="index.php">الرئيسية </a></li>
+                        <li class="breadcrumb-item"><span> <?= $_SESSION['ar_name'] ?> : لوحة القيادة </span></li>
 
-          </ol>
-        </div>
-      </nav>
+                    </ol>
+                </div>
+            </nav>
 
 
-      <div class="container">
-        <div class="row">
-          <div class="single-top-popular-course d-flex flex-wrap mb-30 wow fadeInUp" data-wow-delay="400ms">
-            <div class="col-lg-3">
-              <div class="dashbord-list">
-                <ul>
-                  <li class="active">
-                    <a href="javascript:void(0)">
-                      <i class="fa fa-list"></i> الدورات الملتحق بها
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#">
-                      <!-- updateInf.php -->
-                      <i class="fa fa-id-card"></i> معلومات شخصية
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-            <div class="col-lg-9">
-              <div class="dashbord-section">
-                <?php
+            <div class="container">
+                <div class="row">
+                    <div class="single-top-popular-course d-flex flex-wrap mb-30 wow fadeInUp" data-wow-delay="400ms">
+                        <div class="col-lg-3">
+                            <div class="dashbord-list">
+                                <ul>
+                                    <li class="active">
+                                        <a href="javascript:void(0)">
+                                            <i class="fa fa-list"></i> الدورات الملتحق بها
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            <!-- updateInf.php -->
+                                            <i class="fa fa-id-card"></i> معلومات شخصية
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-lg-9">
+                            <div class="dashbord-section">
+                                <?php
                 $i = 0;
 
                 $array['user_id'] = $_SESSION['user'];
@@ -81,15 +81,15 @@ if (isset($_GET['putAtGroup'])) {
                   foreach ($courses_reg as $courseReg) : $i++;
                     include 'reg_to_group.php';
                   endforeach; ?>
-              </div>
-            </div>
-          </div>
-        </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
-      </div>
+            </div>
+        </div>
     </div>
-  </div>
-  <?php require_once("footer.php"); ?>
+    <?php require_once("footer.php"); ?>
 </body>
 
 </html>
